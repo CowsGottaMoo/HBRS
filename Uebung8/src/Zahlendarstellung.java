2091b7d3352c25aabbf9447744989007d7aecbea
CowsGottaMoo <kindt.justus@gmail.com>
2024-11-27 09:23:57 +0100

    test 2

--- Uebung8/src/Zahlendarstellung.java
+++ Uebung8/src/Zahlendarstellung.java
@@ -9,7 +9,7 @@ public class Zahlendarstellung {
     }
 
     public static int[] ermittleZiffern(int x, int b) {
-        //kommentar
+        //kommentar 2
         int[] ziffern = new int[ermittleLaenge(x, b)];
         if (b <= 1) {
             return null;
