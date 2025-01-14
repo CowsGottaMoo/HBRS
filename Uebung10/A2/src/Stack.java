08abc57ff09518c0527708ce41138847c438c8a7
Justus <kindt.justus@gmail.com>
2025-01-14 10:56:34 +0100

    stack

---
+++ Uebung10/A2/src/Stack.java
@@ -0,0 +1,21 @@
+
+public class Stack {
+
+    private int maximalGroesse;
+
+    public Stack(int maximalGroesse) {
+        this.maximalGroesse = maximalGroesse;
+    }
+
+    public void push(Object o) {
+
+    }
+
+    public Object pop() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+}
